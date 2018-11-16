1315cc411fd103ac546698a2d2f48fdd3b6d1d61
Derek Duafa <Derek.Duafa@gcnetghana.com>
2018-11-16 17:30:43 +0000

    Base Application

---
+++ backend/data.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DataSchema = new Schema(
+    {
+        id: Number,
+        message:String
+    },
+    { timestamp: true }
+);
+
+module.exports = mongoose.model("Data", DataSchema);
